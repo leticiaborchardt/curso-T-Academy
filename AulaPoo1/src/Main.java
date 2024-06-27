76107a33b146352b3b5049c2552eadf509d2b207
Leticia Borchardt <leticiaborchardt123@gmail.com>
2024-06-26 21:32:31 -0300

    Aula 26/06 - conceito de encapsulamento

--- AulaPoo1/src/Main.java
+++ AulaPoo1/src/Main.java
@@ -2,13 +2,19 @@ import exercicios.Aluno;
 
 public class Main {
     public static void main(String[] args) {
-        Aluno aluno1 = new Aluno("Letícia", 19, "T-Academy");
-        Aluno aluno2 = new Aluno("João", 23, "Desenvolvimento WEB");
-        Aluno aluno3 = new Aluno("Fernanda", 17, "T-Academy");
+        Aluno aluno1 = new Aluno("Letícia", 19, "T-Academy", "a-5468584");
+        aluno1.setMedia(8);
+
+        Aluno aluno2 = new Aluno("João", 23, "Desenvolvimento WEB", "a-5464888");
+        Aluno aluno3 = new Aluno("Fernanda", 17, "T-Academy", "a-5468900");
 
         Aluno[] alunos = new Aluno[3];
         alunos[0] = aluno1;
         alunos[1] = aluno2;
         alunos[2] = aluno3;
+
+        for (Aluno aluno : alunos) {
+            aluno.imprimir();
+        }
     }
 }
--- AulaPoo1/src/exercicios/Aluno.java
+++ AulaPoo1/src/exercicios/Aluno.java
@@ -4,12 +4,44 @@ public class Aluno {
     public String nome;
     public int idade;
     public String turma;
+    private String matricula;
+    private double media;
 
     public Aluno() {}
 
-    public Aluno(String nome, int idade, String turma) {
+    public Aluno(String nome, int idade, String turma, String matricula) {
         this.nome = nome;
         this.idade = idade;
         this.turma = turma;
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public void imprimir() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Turma: " + this.turma);
+        System.out.println("Matrícula: " + this.matricula);
+        System.out.println("Situação: " + (this.isAprovado() ? "Aprovado" : "Reprovado"));
+        System.out.println("------");
+    }
+
+    private boolean isAprovado() {
+        return this.media >= 6;
     }
 }

7fa99889b95edea7e150e4d740351c1b4cd76fac
Leticia <leticiaborchardt123@gmail.com>
2024-08-21 20:46:17 -0300

    Adicionando projeto para o exercício de request params

---
+++ Exercicios/requestparams/src/test/java/com/exercicios/requestparams/RequestparamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicios.requestparams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestparamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
